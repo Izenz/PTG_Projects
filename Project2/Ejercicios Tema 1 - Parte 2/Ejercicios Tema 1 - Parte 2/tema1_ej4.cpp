6771649387e44b60c0a3f49203f9f3f024f75fc4
Joel <jherraizmarti@gmail.com>
2020-02-17 16:23:42 +0100

    Exercise 5 finished

--- Project2/Ejercicios Tema 1 - Parte 2/Ejercicios Tema 1 - Parte 2/tema1_ej4.cpp
+++ Project2/Ejercicios Tema 1 - Parte 2/Ejercicios Tema 1 - Parte 2/tema1_ej4.cpp
@@ -17,7 +17,7 @@ void mouseMotion(int, int);
 bool fullscreen = false;
 bool mouseDown = false;
 bool animation = true;
- 
+
 float xrot = 0.0f;
 float yrot = 0.0f;
 float xdiff = 0.0f;
@@ -27,26 +27,29 @@ int g_Width = 500;                          // Ancho inicial de la ventana
 int g_Height = 500;                         // Altura incial de la ventana
 
 // Intensidad de la luz
-const GLfloat intensidadLuz[] = {1.0f, 1.0f, 1.0f, 1.0f};	
+const GLfloat intensidadLuz[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 // Posición de la luz
-const GLfloat luzLocal[] = {0.5f, 0.5f, 1.0f, 1.0f}; // Tanto la luz puntual como el foco
-const GLfloat luzDireccional[] = {0.5f, 0.5f, 1.0f, 0.0f}; // Luz direccional
+const GLfloat luzLocal[] = { 0.5f, 0.5f, 1.0f, 1.0f }; // Tanto la luz puntual como el foco
+const GLfloat luzDireccional[] = { 0.5f, 0.5f, 1.0f, 0.0f }; // Luz direccional
 // Dirección a la que apunta el foco
-const GLfloat direccionFoco[] = {-0.5f, -0.5f, -4.0f}; // para que apunte al centro escena: dir =  -pos(luzLocal) - pos(lookat)
+const GLfloat direccionFoco[] = { -0.5f, -0.5f, -4.0f }; // para que apunte al centro escena: dir =  -pos(luzLocal) - pos(lookat)
 // Amplitud del cono de luz del foco (en grados)
 const GLfloat amplitudFoco = 10.0f; //grados
 
 // Id de la textura
-GLuint texture_id; 
+GLuint texture_id;
 
 GLboolean lightningActivated = false;
 GLboolean smoothingActivated = false;
 GLboolean textureActivated = false;
+GLboolean textPasteMode = true;
+GLboolean textRepeatMode = false;
 GLint activeLight = 0;
+GLint activeFilter = 0;
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	glutInit(&argc, argv); 
+	glutInit(&argc, argv);
 	glutInitWindowPosition(50, 50);
 	glutInitWindowSize(g_Width, g_Height);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
@@ -60,30 +63,30 @@ int main(int argc, char *argv[])
 	glutMotionFunc(mouseMotion);
 	glutReshapeFunc(resize);
 	glutIdleFunc(idle);
- 
+
 	glutMainLoop();
- 
+
 	return EXIT_SUCCESS;
 }
- 
+
 
 void drawPlane()
 {
 	const int numDivisiones = 100;
 	const int TAM = 3.0f;
-	for (int i = 0; i < numDivisiones; i ++ )
+	for (int i = 0; i < numDivisiones; i++)
 	{
 		float y0 = TAM * float(i) / numDivisiones - TAM / 2.0f;
-		float yf = TAM * float(i+1) / numDivisiones - TAM / 2.0f;
+		float yf = TAM * float(i + 1) / numDivisiones - TAM / 2.0f;
 		glBegin(GL_TRIANGLE_STRIP);
-		for ( int j = 0; j <= numDivisiones; j ++ )
+		for (int j = 0; j <= numDivisiones; j++)
 		{
 			float x = TAM * float(j) / numDivisiones - TAM / 2.0f;
-			// glTexCoord2f(x,y0);
-			// glNormal3f(0.0f, 0.0f, 1.0f);
+			glTexCoord2f(x, y0);
+			glNormal3f(0.0f, 0.0f, 1.0f);
 			glVertex3f(x, y0, 0.0f);
-			// glTexCoord2f(x,yf);
-			// glNormal3f(0.0f, 0.0f, 1.0f);
+			glTexCoord2f(x, yf);
+			glNormal3f(0.0f, 0.0f, 1.0f);
 			glVertex3f(x, yf, 0.0f);
 		}
 		glEnd();
@@ -93,14 +96,14 @@ void drawPlane()
 bool init()
 {
 	glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
- 
+
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 	glClearDepth(1.0f);
 
 	// Tarea por hacer: Activar la iluminación.
 	// glEnable(GL_LIGHTING);
-	
+
 	// Tarea por hacer: definir 3 luces.
 	// 1º luz: puntual
 	glLightfv(GL_LIGHT0, GL_POSITION, luzLocal);
@@ -115,7 +118,7 @@ bool init()
 	glLightfv(GL_LIGHT1, GL_AMBIENT, intensidadLuz);
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, intensidadLuz);
 	glLightfv(GL_LIGHT2, GL_SPECULAR, intensidadLuz);
-	
+
 	// 3ª luz: Direccional
 	glLightfv(GL_LIGHT2, GL_POSITION, luzDireccional);
 	glLightfv(GL_LIGHT2, GL_AMBIENT, intensidadLuz);
@@ -123,39 +126,39 @@ bool init()
 	glLightfv(GL_LIGHT2, GL_SPECULAR, intensidadLuz);
 
 	glEnable(GL_LIGHT0);
-	
+
 	// Tarea por hacer: Crear el obj. textura, cargar la imagen tga, activar la textura, cargar los datos en el objeto textura y definir filtros, modo repetición y modo pegado
 	// Inicializa Texturas
 	TGAFILE tgaImage[1];
-	glGenTextures (1, &texture_id);  
+	glGenTextures(1, &texture_id);
 	std::cout << "Cargando la textura metalSheetDiffuse.tga" << std::endl;
 	char pc[] = "metalSheetDiffuse.tga";
-	if ( LoadTGAFile(pc, &tgaImage[0]) )
-    {
-	   glBindTexture(GL_TEXTURE_2D, texture_id);
-	   
-	   std::cout << "tamanyo (w,h) = (" << tgaImage[0].imageWidth << "," << tgaImage[0].imageHeight << ")" << std::endl;
-	   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tgaImage[0].imageWidth, tgaImage[0].imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, tgaImage[0].imageData);
-		// gluBuild2DMipmaps(GL_TEXTURE_2D,			// texture to specify
-		//				 GL_RGBA,				// internal texture storage format
-		//				 tgaImage[0].imageWidth,   // texture width
-		//				 tgaImage[0].imageHeight,	// texture height
-		//				 GL_RGBA,				// pixel format
-		//				 GL_UNSIGNED_BYTE,		// color component format
-		//				 tgaImage[0].imageData);	// pointer to texture image
+	if (LoadTGAFile(pc, &tgaImage[0]))
+	{
+		glBindTexture(GL_TEXTURE_2D, texture_id);
 
-	   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	   glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPEAT);
-    }
-   else
-	   std::cout << "Error al cargar la textura metalSheetDiffuse.tga" << std::endl;
+		std::cout << "tamanyo (w,h) = (" << tgaImage[0].imageWidth << "," << tgaImage[0].imageHeight << ")" << std::endl;
+		//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tgaImage[0].imageWidth, tgaImage[0].imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, tgaImage[0].imageData);
+		gluBuild2DMipmaps(GL_TEXTURE_2D,			// texture to specify
+						 GL_RGBA,				// internal texture storage format
+						 tgaImage[0].imageWidth,   // texture width
+						 tgaImage[0].imageHeight,	// texture height
+						 GL_RGBA,				// pixel format
+						 GL_UNSIGNED_BYTE,		// color component format
+						 tgaImage[0].imageData);	// pointer to texture image
+
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	}
+	else
+		std::cout << "Error al cargar la textura metalSheetDiffuse.tga" << std::endl;
 
 	return true;
 }
- 
+
 void display()
 {
 	GLfloat colorAmbient[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
@@ -165,15 +168,15 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
- 
+
 	gluLookAt(
-	0.0f, 0.0f, 3.0f,
-	0.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f);
- 
+		0.0f, 0.0f, 3.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+
 	glRotatef(xrot, 1.0f, 0.0f, 0.0f);
 	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
- 
+
 	if (lightningActivated) {
 		glEnable(GL_LIGHTING);
 	}
@@ -182,7 +185,7 @@ void display()
 	}
 
 	glShadeModel(smoothingActivated ? GL_SMOOTH : GL_FLAT);
-	
+
 	if (textureActivated) {
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, texture_id);
@@ -197,9 +200,9 @@ void display()
 	glMaterialfv(GL_FRONT, GL_SPECULAR, colorBronzeSpec);
 	glMaterialf(GL_FRONT, GL_SHININESS, 50.0f);
 	//glColor4fv(colorBronzeDiff); // Color de los objetos dibujados en la escena
-	
-	
-	
+
+
+
 	glutSolidTeapot(0.5f);
 	drawPlane();
 	//glutWireTeapot(0.5f);
@@ -216,7 +219,7 @@ void display()
 	glFlush();
 	glutSwapBuffers();
 }
- 
+
 void resize(int w, int h)
 {
 	glMatrixMode(GL_PROJECTION);
@@ -225,13 +228,13 @@ void resize(int w, int h)
 	g_Width = w;
 	g_Height = h;
 	glViewport(0, 0, g_Width, g_Height);
- 
+
 	gluPerspective(45.0f, 1.0f * g_Width / g_Height, 1.0f, 100.0f);
- 
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
- 
+
 void idle()
 {
 	if (!mouseDown && animation)
@@ -239,25 +242,25 @@ void idle()
 		xrot += 0.3f;
 		yrot += 0.4f;
 	}
- 
+
 	glutPostRedisplay();
 }
- 
+
 void keyboard(unsigned char key, int x, int y)
 {
-	switch(key)
+	switch (key)
 	{
-	case 27 : case 'q': case 'Q':
-		exit(1); 
+	case 27: case 'q': case 'Q':
+		exit(1);
 		break;
 	case 'a': case 'A':
 		animation = !animation;
 		break;
-	case 'l': case 'L': 
+	case 'l': case 'L':
 		// Tarea por hacer: activar/desctivar la iluminación.
 		lightningActivated = !lightningActivated;
 		break;
-	case 's': case 'S': 
+	case 's': case 'S':
 		// Tarea por hacer: cambiar el modo de sombreado (de suave a plano y viceversa).
 		smoothingActivated = !smoothingActivated;
 		break;
@@ -282,28 +285,54 @@ void keyboard(unsigned char key, int x, int y)
 			break;
 		}
 		break;
-	case 't': case 'T': 
+	case 't': case 'T':
 		// Tarea por hacer: activa/desactiva textura
 		textureActivated = !textureActivated;
 		break;
 	case 'm': case 'M':
 		// Tarea por hacer: modo de pegado de la textura
+		textPasteMode = !textPasteMode;
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, textPasteMode ? GL_REPLACE : GL_MODULATE);
 		break;
 	case 'e': case 'E':
 		// Tarea por hacer: modo de repetición de la textura
+		textRepeatMode = !textRepeatMode;
+
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, textRepeatMode ? GL_CLAMP : GL_REPEAT);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, textRepeatMode ? GL_CLAMP : GL_REPEAT);
+		
 		break;
 	case 'f': case 'F':
 		// Tarea por hacer: cambiar el filtro de minificación de la textura
-		break;	
+		++activeFilter;
+		if (activeFilter > 2)
+			activeFilter = 0;
+
+		switch (activeFilter) {
+		case 0:
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			break;
+		case 1:
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			break;
+		case 2:
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			break;
+		}
+		
+		break;
 	}
 }
- 
+
 void specialKeyboard(int key, int x, int y)
 {
 	if (key == GLUT_KEY_F1)
 	{
 		fullscreen = !fullscreen;
- 
+
 		if (fullscreen)
 			glutFullScreen();
 		else
@@ -313,27 +342,27 @@ void specialKeyboard(int key, int x, int y)
 		}
 	}
 }
- 
+
 void mouse(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
 		mouseDown = true;
- 
+
 		xdiff = x - yrot;
 		ydiff = -y + xrot;
 	}
 	else
 		mouseDown = false;
 }
- 
+
 void mouseMotion(int x, int y)
 {
 	if (mouseDown)
 	{
 		yrot = x - xdiff;
 		xrot = y + ydiff;
- 
+
 		glutPostRedisplay();
 	}
 }
